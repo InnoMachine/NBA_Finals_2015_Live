67db3b99d0caf9ae20f4fde0cd29b5ced5792969
InnoMachine <jx13@software.nju.edu.cn>
2015-06-15 04:41:27 +0800

    add

---
+++ src/po/LiveTexts.java
@@ -0,0 +1,28 @@
+package po;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class LiveTexts extends Observable{
+
+	private ArrayList<LiveTextPO> liveTexts;
+	
+	public LiveTexts() {
+		this.setLiveTexts(null);
+	}
+	
+	public LiveTexts(ArrayList<LiveTextPO> liveTexts) {
+		this.setLiveTexts(liveTexts);
+	}
+
+	public ArrayList<LiveTextPO> getLiveTexts() {
+		return liveTexts;
+	}
+
+	public void setLiveTexts(ArrayList<LiveTextPO> liveTexts) {
+		this.liveTexts = liveTexts;
+		setChanged();
+		notifyObservers(liveTexts);
+	}
+
+}
